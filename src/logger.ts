ee5c588499c64aed99f25e34436b197a7ebbef59
Matthew Wong <matwong@zoho.com>
2019-06-26 09:39:06 -0400

    minor adj on module resultion and additonal tsconfig

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,13 @@
 // TODO: include line number to customer logger using the error stack trick
 import * as winston from 'winston'
 // const { combine, timestamp, label, prettyPrint } = winston.format
+const format = winston.format
+const transports = winston.transports
 
 export default class CutomLogger {
   public static logger = winston.createLogger({
     level: 'info',
-    format: winston.format.simple(),
-    transports: [new winston.transports.Console()],
+    format: format.simple(),
+    transports: [new transports.Console()],
   })
 }
