457b1854d1e7eee0cf4c822738edc28588be2559
Matthew Wong <matwong@zoho.com>
2019-07-27 13:15:56 -0400

    removes redundant http. uses express instead

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import depthLimit from 'graphql-depth-limit'
-import { createServer } from 'http'
 import cors from 'cors'
 import compression from 'compression'
 
@@ -17,14 +16,14 @@ const server = new ApolloServer({
   playground: true,
   validationRules: [depthLimit(7)],
 })
+
 app.use('*', cors())
 app.use(compression())
 server.applyMiddleware({ app, path: '/graphql' })
-const httpServer = createServer(app)
 
 // basic old fashion REST route
 app.get('/', (req, res) => res.send('Hello World!'))
 
-httpServer.listen({ port: 4000 }, () =>
+app.listen({ port: 4000 }, () =>
   logger.info(`\n🚀      GraphQL is now running on http://localhost:4000/graphql`),
 )
