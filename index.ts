09761549cbeb9f7dd830f172099bcb7ffda29187
Matthew Wong <matwong@zoho.com>
2019-08-03 20:14:28 -0400

    adds req/res to apollo express server

--- index.ts
+++ index.ts
@@ -12,6 +12,11 @@ const app = express()
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req, res }) => ({
+    headers: req.headers,
+    req,
+    res,
+  }),
   introspection: true,
   playground: true,
   // validationRules: [depthLimit(7)],
